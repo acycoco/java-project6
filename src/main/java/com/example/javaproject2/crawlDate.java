aff0c3429d8dd27bb143393d03877da414bca8ac
LUCKYCOM\space <pink10257@naver.com>
2023-04-19 13:10:06 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject2/crawlDate.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2;
+
+import java.time.LocalDateTime;
+
+public class crawlDate {
+    private LocalDateTime lateCrawlDateTime;
+
+    public crawlDate(LocalDateTime lateCrawlDateTime) {
+        this.lateCrawlDateTime = lateCrawlDateTime;
+    }
+
+
+}
