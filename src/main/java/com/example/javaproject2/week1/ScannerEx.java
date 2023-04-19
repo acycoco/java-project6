7c7205d5a5a033d9b82e8be6772e6cc044a32857
LUCKYCOM\space <pink10257@naver.com>
2023-04-19 15:32:08 +0900

    feat: ScannerEx 추가 -ScannerEx, ScannerExTest 클래스 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerEx.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void plusTwoNumbers(int val1, int val2){
+
+    }
+
+    public void readTwoNumbersAndPlus(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + scanner.nextInt());
+    }
+}
