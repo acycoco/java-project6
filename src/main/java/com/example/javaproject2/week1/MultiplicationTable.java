9cb9b69eb55a23124f1d0aab78e8f523c7fb9961
LUCKYCOM\space <pink10257@naver.com>
2023-04-20 13:07:37 +0900

    feat: printf를 이용한 곱하기, 날짜,hello 출력 -MultiplicationTable, PrintfDate, PrintWhy 클래스 추가

---
+++ src/main/java/com/example/javaproject2/week1/MultiplicationTable.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        System.out.printf("%d * %d = %d\n", 2, 2, 4);
+        System.out.printf("%d * %d = %d\n", 2, 2, 2 * 2);
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/PrintWhy.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week1;
+
+public class PrintWhy {
+    public void print(){
+        System.out.printf("%s", "Hello\n");
+
+    }
+
+    public static void main(String[] args) {
+        PrintWhy printWhy = new PrintWhy();
+        printWhy.print();
+    }
+}
