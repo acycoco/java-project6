847204c317d6b3c300564ad891421acc0b80bb55
LUCKYCOM\space <pink10257@naver.com>
2023-04-20 09:41:11 +0900

    feat: 문자열 두개 더하기 -ScannerStrEx, ScannerStrExTest class 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerStrExTest.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week1;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx sse = new ScannerStrEx();
+        sse.inputStringsAndPlus();
+
+    }
+}
