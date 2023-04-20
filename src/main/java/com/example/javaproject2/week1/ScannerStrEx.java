5e98c44a1aa83a62ae86734f6eec76e363fb36e1
LUCKYCOM\space <pink10257@naver.com>
2023-04-20 10:37:01 +0900

    feat: 두개의 숫자 입력받고 출력하기 -ScannerStrEx 클래스 사용

--- src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
+++ src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class ScannerStrEx {
     void inputStringsAndPlus(){
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.next() + sc.next());
+        System.out.println("두개의 숫자를 입력하세요. 입력하고 엔터 입력하고 엔터");
+        System.out.print("첫번째 숫자:");
+        System.out.println("첫번째 숫자는 " + sc.next() + "입니다. ");
+        System.out.print("두번째 숫자:");
+        System.out.println("두번째 숫자는 " + sc.next() + "입니다. ");
 
     }
 }
