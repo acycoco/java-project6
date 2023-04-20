7df14d74bb9812574b5a58d0164616dd39090e6e
LUCKYCOM\space <pink10257@naver.com>
2023-04-20 15:35:36 +0900

    feat: codeup 11 ~ 14번 풀이 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1014.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1014 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char c1 = sc.next().charAt(0);
+        char c2 = sc.next().charAt(0);
+
+        System.out.printf("%c %c", c2, c1);
+
+    }
+}
