b89203de42b7962bdfbc3da38dc50a1c972a4be6
LUCKYCOM\space <pink10257@naver.com>
2023-04-20 13:05:20 +0900

    feat: codeup 10번 풀이 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1010.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Codeup1010 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt());
+
+    }
+}
