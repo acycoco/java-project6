7911a8d340a0e1e7d1a7e2607ec49a27ccdec39e
LUCKYCOM\space <pink10257@naver.com>
2023-04-20 09:31:08 +0900

    feat: 두개 숫자 더하기 -ScannerTwoNumbers, ScannerReaderTwoNumbersTest class 추가

---
+++ src/main/java/com/example/javaproject2/week1/ScannerReaderTwoNumbersTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+public class ScannerReaderTwoNumbersTest {
+    public static void main(String[] args) {
+        ScannerTwoNumbers stn = new ScannerTwoNumbers();
+        stn.plusTwoNums();
+
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/ScannerTwoNumbers.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerTwoNumbers {
+    public void plusTwoNums() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() + sc.nextInt());
+
+
+
+
+
+    }
+}
