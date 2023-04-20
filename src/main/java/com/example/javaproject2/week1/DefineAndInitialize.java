485a710c3b9d6dc861451d463d0a6cda5eb17869
LUCKYCOM\space <pink10257@naver.com>
2023-04-20 15:33:09 +0900

    feat: 변수 정의하고 초기화하는 실습 -DefineAndInitialize, ScannerAndVariable, StoreValueAndPrint 클래스 추가

---
+++ src/main/java/com/example/javaproject2/week1/DefineAndInitialize.java
@@ -0,0 +1,17 @@
+package com.example.javaproject2.week1;
+
+import com.example.javaproject2.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int iVal = 1;
+        PrintHello printHello = new PrintHello();
+
+
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/StoreValueAndPrint.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class StoreValueAndPrint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iVal1 = sc.nextInt();
+        int iVal2 = sc.nextInt();
+        System.out.println(iVal1 + iVal2);
+        System.out.printf("%d + %d = %d",iVal1, iVal2, iVal1 + iVal2);
+
+    }
+}
