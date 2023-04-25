2d9fc4e93957059b855eeae9fe7b8cf46449a1f7
LUCKYCOM\space <pink10257@naver.com>
2023-04-25 09:24:13 +0900

    feat: week2 day2 - codup 1032 ~ 1037

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1036.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1036 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+        System.out.printf("%d\n", (int)c);
+
+
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1037.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1037 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int iVal = sc.nextInt();
+        System.out.printf("%c\n", (char)iVal);
+
+
+
+    }
+}
