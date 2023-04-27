dee902d194447ce086edd39c7eb3d8a4b412fe4e
LUCKYCOM\space <pink10257@naver.com>
2023-04-27 10:17:26 +0900

    feat: 증감연산자 실습 추가

---
+++ src/main/java/com/example/javaproject2/wekk2/day3/DecreaseOperator.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.wekk2.day3;
+
+public class DecreaseOperator {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i--);
+        System.out.println(i);
+    }
+}
