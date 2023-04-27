4e56d0b22644c73e496ee7d035821ca5c8df4858
LUCKYCOM\space <pink10257@naver.com>
2023-04-27 13:14:14 +0900

    feat: And, Or, Not 연산자 실습 추가

---
+++ src/main/java/com/example/javaproject2/wekk2/day4/NotOperatorEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.wekk2.day4;
+
+public class NotOperatorEx {
+    public static void main(String[] args) {
+        boolean result = !true;
+        System.out.printf("%b\n", result);
+        result = !false;
+        System.out.printf("%b\n", result);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/wekk2/day4/OrOperatorEx.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.wekk2.day4;
+
+public class OrOperatorEx {
+    public static void main(String[] args) {
+        boolean result = true || true;
+        System.out.printf("%b\n", result);
+        result = true || false;
+        System.out.printf("%b\n", result);
+        result = false || true;
+        System.out.printf("%b\n", result);
+        result = false  || false;
+        System.out.printf("%b\n", result);
+    }
+}
