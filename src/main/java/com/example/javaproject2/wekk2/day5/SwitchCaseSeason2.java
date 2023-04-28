06d9ef89120e700ddf7e8f5c0294caea604233cd
LUCKYCOM\space <pink10257@naver.com>
2023-04-28 14:16:11 +0900

    feat: 4/28 실습 Switch문, 병원진료시간 출력 추가

---
+++ src/main/java/com/example/javaproject2/wekk2/day5/SwitchCaseSeason2.java
@@ -0,0 +1,21 @@
+package com.example.javaproject2.wekk2.day5;
+
+public class SwitchCaseSeason2 {
+    public static void main(String[] args) {
+        int month = 11;
+        switch(month) {
+            case 12, 1, 2: System.out.println("겨울"); break; //이런 식으로 한줄에 써도 됨
+            case 3, 4, 5:
+                System.out.println("봄");
+                break;
+            case 6, 7, 8:
+                System.out.println("여름");
+                break;
+            case 9, 10, 11:
+                System.out.println("가을");
+                break;
+            default:
+                System.out.println("해당하는 계절이 없습니다.");
+        }
+    }
+}
