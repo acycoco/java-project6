9905646b2c6665ba98564ce6ca9b8c3ea8fa3c6f
LUCKYCOM\space <pink10257@naver.com>
2023-04-28 11:42:21 +0900

    feat: 4/28 실습 Switch까지 추가

---
+++ src/main/java/com/example/javaproject2/wekk2/day4/BitOperatorEx2.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.wekk2.day4;
+
+public class BitOperatorEx2 {
+    public static void main(String[] args) {
+        int iVal1 = 3;
+        System.out.println(~iVal1);
+        System.out.println(Integer.toBinaryString(~iVal1));
+        System.out.println(Integer.toBinaryString(iVal1));
+    }
+}
