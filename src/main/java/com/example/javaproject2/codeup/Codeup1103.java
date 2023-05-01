04cc396e76569109f7a2ec012756c99390fda35c
LUCKYCOM\space <pink10257@naver.com>
2023-05-01 10:51:54 +0900

    feat: codeup 추가, newSwitch문, for문 추가 - codeup 1071,72, 1103, 1106 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1103.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+
+public class Codeup1103 {
+    public static void main(String[] args) {
+
+
+
+        System.out.printf("\"c:\\test\"");
+
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1106.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+
+public class Codeup1106 {
+    public static void main(String[] args) {
+
+
+
+        System.out.printf("%d %d", 0x80000000, 0x7fffffff);
+
+
+    }
+}
